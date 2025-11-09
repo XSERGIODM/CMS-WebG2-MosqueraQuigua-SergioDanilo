b427aa51b5028282b65b17fdb583293ac15fb074
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-11-09 18:03:46 -0500

    Implementa página de detalle de noticia
    Añade la página completa para visualizar el detalle de una noticia.
    Incluye un diseño responsivo con metadatos, galería de imágenes, opciones para compartir en redes sociales y una sección de noticias relacionadas.
    La lógica del componente utiliza señales y propiedades computadas de Angular para una gestión eficiente de los datos.
    Además, se expande la configuración de rutas para soportar la navegación a esta página y otras relacionadas con noticias, mejorando la experiencia de usuario.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './page/home-page/home-page';
+import {NoticiasPage} from './page/noticias-page/noticias-page';
+import {DetalleNoticiaPage} from './page/detalle-noticia-page/detalle-noticia-page';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomePage
+  },
+  {
+    path: 'noticias',
+    component: NoticiasPage
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detalle-noticia/:id',
+    component: DetalleNoticiaPage
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ];
