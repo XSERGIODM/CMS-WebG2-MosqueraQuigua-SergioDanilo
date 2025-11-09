ddd1d834fc823e752eb5bdee602345f43815615a
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-11-09 18:03:32 -0500

    Implementa página de noticias con filtrado y paginación
    Añade una sección completa de noticias con búsqueda avanzada.
    Permite filtrar noticias por categoría y ordenar por fecha o título.
    Incluye paginación dinámica para mejorar la navegación entre los artículos.
    Actualiza la barra de navegación para usar `routerLink` y dirigir a la nueva página de noticias.
    Utiliza Angular Signals para gestionar el estado y la reactividad de los filtros y la paginación.

--- src/app/components/navbar-componen/navbar-componen.ts
+++ src/app/components/navbar-componen/navbar-componen.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar-componen',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar-componen.html',
   styleUrl: './navbar-componen.css',
 })
