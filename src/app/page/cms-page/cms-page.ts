dbd05e615ea863becedac6d54fcd1abd6b74b6f4
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-11-09 09:35:11 -0500

    Inicializa la estructura básica del proyecto
    Establece la estructura inicial del proyecto Angular, incluyendo:
    
    - Reemplaza el HTML predeterminado de la aplicación con una estructura
      básica que incluye componentes de navegación y pie de página.
    - Configura el entorno de desarrollo para usar un archivo de entorno
      específico.
    - Agrega modelos para representar datos como categorías, noticias y
      usuarios.
    - Crea componentes de página básicos para la administración, gestión
      de contenido (CMS) y perfiles de usuario.
    - Inicializa la configuración de Firebase para la conexión con la base de datos.

---
+++ src/app/page/cms-page/cms-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cms-page',
+  imports: [],
+  templateUrl: './cms-page.html',
+  styleUrl: './cms-page.css',
+})
+export class CmsPage {
+
+}
---
+++ src/firebase/firbaseConfig.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {environment} from '../environments/environment';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = environment.firebase;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
