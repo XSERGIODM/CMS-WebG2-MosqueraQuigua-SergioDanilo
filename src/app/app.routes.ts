3f40c92ca06252776f2bab313653554adc0c46a4
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-11-09 19:29:10 -0500

    Implementa la autenticación de usuarios con Firebase
    Integra AngularFire para habilitar la autenticación de usuarios usando Firebase Authentication y Firestore.
    
    Añade las siguientes funcionalidades:
    - Configuración de Firebase en `app.config.ts`.
    - Nuevas rutas para las páginas de inicio de sesión (`/login`) y perfil de usuario (`/perfil`).
    - Páginas de `LoginPage` y `PerfilPage` con formularios de login, registro y edición de perfil.
    - Un `AuthService` para gestionar el estado de la autenticación, el registro de usuarios, el inicio y cierre de sesión.
    - Actualización de la barra de navegación para mostrar opciones dinámicas (Iniciar Sesión / Mi Perfil) según el estado de autenticación del usuario.
    - Exclusión de archivos de configuración de Firebase (`firestore.rules`, `firebase-debug.log`) en `.gitignore`.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomePage } from './page/home-page/home-page';
 import {NoticiasPage} from './page/noticias-page/noticias-page';
 import {DetalleNoticiaPage} from './page/detalle-noticia-page/detalle-noticia-page';
+import {LoginPage} from './page/login-page/login-page';
+import {PerfilPage} from './page/perfil-page/perfil-page';
 
 export const routes: Routes = [
   {
@@ -20,6 +22,14 @@ export const routes: Routes = [
   {
     path: 'detalle-noticia/:id',
     component: DetalleNoticiaPage
+  },
+  {
+    path: 'login',
+    component: LoginPage
+  },
+  {
+    path: 'perfil',
+    component: PerfilPage
   },
   {
     path: '**',
