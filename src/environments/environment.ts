2b5a2dffdd588682069d032fc84661755b77b877
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2025-11-09 23:59:43 -0500

    Establece credenciales de Firebase directamente
    Anteriormente, las credenciales de Firebase se recuperaban de variables de entorno. Este cambio integra directamente las claves de configuración de Firebase en los archivos `environment.ts` y `environment.prod.ts`.
    
    Esto simplifica la configuración del entorno para el desarrollo y la producción, eliminando la dependencia de variables de entorno externas.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-    appId: process.env['FIREBASE_APP_ID']
+    apiKey: "AIzaSyAHAr9ADs_dKEY4YUl3jxDyN9-euZsQEeM",
+
+    authDomain: "cms-app-web-firebase.firebaseapp.com",
+
+    projectId: "cms-app-web-firebase",
+
+    storageBucket: "cms-app-web-firebase.firebasestorage.app",
+
+    messagingSenderId: "231113383631",
+
+    appId: "1:231113383631:web:b83cafb6c51bbcba8461ba"
+
   }
 };
